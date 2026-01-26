de08da458f5a959ef1a8970939598d575eb2345e
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-26 23:10:50 +0400

    add Course entity

--- src/main/java/com/example/universitycourseregistration/entity/Course.java
+++ src/main/java/com/example/universitycourseregistration/entity/Course.java
@@ -1,4 +1,48 @@
 package com.example.universitycourseregistration.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Courses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Long id;
+
+    @NotNull(message = "Zəhmət olmasa xananı doldurun, boş saxlamaq olmaz.")
+    @Column(name = "courseName")
+    @Size(min = 1, max = 50)
+    private String courseName;
+
+    @NotEmpty(message = "Zəhmət olmasa xananı doldurun.")
+    @Column (name = "courseDescription")
+    private String courseDescription;
+
+    @NotNull(message = "Zəhmət olmasa xananı doldurun, boş saxlamaq olmaz.")
+    @Column (name = "credits")
+    private int credits;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id") //teacher ile elaqe
+    private Teacher teacher;
+
+    @OneToMany(mappedBy = "course", //registrations-a baglanmaq ucun elaqe
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Registration> registrations;
+
 }

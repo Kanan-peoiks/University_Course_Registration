546742995b61ffb3b0c735879fc5316ad5e7aa32
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-26 23:10:20 +0400

    update Student entity

--- src/main/java/com/example/universitycourseregistration/entity/Student.java
+++ src/main/java/com/example/universitycourseregistration/entity/Student.java
@@ -8,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "Students")
 @Data
@@ -17,21 +19,26 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private Long id;
 
     @NotNull(message = "Zəhmət olmasa xananı doldurun, boş saxlamaq olmaz.")
-    @Column(name = "StudentName", nullable = false)
+    @Column(name = "studentName", nullable = false)
     @Size(min = 3, max = 15)
     private String name;
 
     @NotNull(message = "Zəhmət olmasa xananı doldurun, boş saxlamaq olmaz.")
-    @Column(name = "StudentSurname", nullable = false)
+    @Column(name = "studentSurname", nullable = false)
     @Size(min = 3, max = 25)
     private String surname;
 
     @Email
     @NotNull(message = "Zəhmət olmasa xananı doldurun, boş saxlamaq olmaz.")
-    @Column(name = "StudentEmail", nullable = false, unique = true)
+    @Column(name = "studentEmail", nullable = false, unique = true)
     private String email;
 
+    @OneToMany(mappedBy = "student", //regostrations-a baglanmaq ucun elaqe
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Registration> registrations;
 }

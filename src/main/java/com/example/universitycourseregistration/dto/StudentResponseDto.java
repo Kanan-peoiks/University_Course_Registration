1af3ce27c39c396c8d2a73f034b23c251193398e
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-27 00:33:12 +0400

    update TeacherResponseDto and StudentResponseDto dto

--- src/main/java/com/example/universitycourseregistration/dto/StudentResponseDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/StudentResponseDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,5 @@ public class StudentResponseDto {
     private String name;
     private String surname;
     private String email;
+    private List<CourseResponseDto> courses;
 }
--- src/main/java/com/example/universitycourseregistration/dto/TeacherResponseDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/TeacherResponseDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +14,5 @@ public class TeacherResponseDto {
     private String name;
     private String surname;
     private String department;
-
+    private List<CourseResponseDto> courses;
 }

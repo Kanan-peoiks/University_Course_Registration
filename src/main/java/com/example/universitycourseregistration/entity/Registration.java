057e36d714ef28207c610a698258cdbe1d97934c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-26 23:11:08 +0400

    add Registration entity

--- src/main/java/com/example/universitycourseregistration/entity/Registration.java
+++ src/main/java/com/example/universitycourseregistration/entity/Registration.java
@@ -1,4 +1,28 @@
 package com.example.universitycourseregistration.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Registration")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "registration_id")
+    private Long registrationId;
+
+    @ManyToOne //Student ile elaqe
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne //course ile elaqe
+    @JoinColumn(name = "course_id")
+    private Course course;
+
 }

8b9e5b4c8798deaa2090f50ec82a8d5b807fc994
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-27 00:00:58 +0400

    update StudentRequestDto and StudentResponseDto dto

--- src/main/java/com/example/universitycourseregistration/dto/StudentRequestDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/StudentRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.universitycourseregistration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentRequestDto {
+    private String name;
+    private String surname;
+    private String email;
 }

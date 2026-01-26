6b08fab987118e576f1e30be08cd58f4ea14bc0b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-27 00:25:19 +0400

    update CourseRequestDto and CourseResponseDto dto

--- src/main/java/com/example/universitycourseregistration/dto/CourseRequestDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/CourseRequestDto.java
@@ -1,4 +1,15 @@
 package com.example.universitycourseregistration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseRequestDto {
+    private String courseName;
+    private String courseDescription;
+    private int credits;
+
 }
--- src/main/java/com/example/universitycourseregistration/dto/CourseResponseDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/CourseResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.universitycourseregistration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseResponseDto {
+    private long id;
+    private String courseName;
+    private String courseCode;
+    private String courseDescription;
 }

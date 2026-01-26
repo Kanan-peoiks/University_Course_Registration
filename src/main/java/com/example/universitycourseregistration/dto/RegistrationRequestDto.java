bfe7d033fea00e03ddbb72c50a4c690ad9f60b43
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-27 00:32:49 +0400

    update RegistrationRequestDto and RegistrationResponseDto dto

--- src/main/java/com/example/universitycourseregistration/dto/RegistrationRequestDto.java
+++ src/main/java/com/example/universitycourseregistration/dto/RegistrationRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.universitycourseregistration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationRequestDto {
+    private Long studentId;
+    private Long courseId;
 }

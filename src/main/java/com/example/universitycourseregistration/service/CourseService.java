4ae89caf04b506da95d9413877c0b82ed1c65a25
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-27 17:51:52 +0400

    update CourseService

--- src/main/java/com/example/universitycourseregistration/service/CourseService.java
+++ src/main/java/com/example/universitycourseregistration/service/CourseService.java
@@ -1,4 +1,31 @@
 package com.example.universitycourseregistration.service;
 
+import com.example.universitycourseregistration.dto.CourseRequestDto;
+import com.example.universitycourseregistration.dto.CourseResponseDto;
+
+import java.util.List;
+
 public interface CourseService {
+
+    //create
+    CourseResponseDto createCourse(CourseRequestDto courseRequestDto);
+
+    //Read
+    List<CourseResponseDto> findAllCourses();
+
+    CourseResponseDto getById(Long id);
+
+    //update
+    CourseResponseDto updateCourse(Long id, CourseRequestDto courseRequestDto);
+
+    //delete
+    void deleteCourse(Long id);
+
+    List<CourseResponseDto> findByTeacherId(Long teacherId);
+
+    List<CourseResponseDto> findByTeacherDepartment(String department);
+
+    List<CourseResponseDto> findByCourseNameContaining(String courseName);
+
+
 }

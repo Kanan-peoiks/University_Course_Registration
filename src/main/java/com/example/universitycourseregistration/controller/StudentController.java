ba9935826b9e0fec728b57a6a6a74621677fdb7d
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-03 02:19:18 +0400

    update StudentController class

--- src/main/java/com/example/universitycourseregistration/controller/StudentController.java
+++ src/main/java/com/example/universitycourseregistration/controller/StudentController.java
@@ -1,4 +1,65 @@
 package com.example.universitycourseregistration.controller;
 
+import com.example.universitycourseregistration.dto.StudentRequestDto;
+import com.example.universitycourseregistration.dto.StudentResponseDto;
+import com.example.universitycourseregistration.service.StudentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+
+    //CREATE
+    @PostMapping
+    public StudentResponseDto create(@RequestBody @Valid StudentRequestDto studentRequestDto) {
+        return studentService.createStudent(studentRequestDto);
+    }
+
+    //READ ALL
+    @GetMapping
+    public List<StudentResponseDto> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    //Read By ID
+    @GetMapping("/{id}")
+    public StudentResponseDto getById(@PathVariable Long id){
+        return studentService.getById(id);
+    }
+
+    //update
+    @PutMapping("/{id}")
+    public StudentResponseDto updateStudent(@PathVariable Long id,
+                               @RequestBody @Valid StudentRequestDto studentRequestDto) {
+        return studentService.updateStudent(id, studentRequestDto);
+    }
+
+    //delete
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+    }
+
+    //find by email
+    @GetMapping("/email")
+    public Optional<StudentResponseDto> findByEmail(@RequestParam String email){
+        return studentService.findByEmail(email);
+    }
+
+    //name + surname
+    @GetMapping("/seacrh")
+    public List<StudentResponseDto> findByNameAndSurname(@RequestParam String name,
+                                                         @RequestParam String surname){
+        return studentService.findByNameAndSurname(name, surname);
+    }
+
+
 }

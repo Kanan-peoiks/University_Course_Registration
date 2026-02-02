3e5e3e4464f8bdf451af9cbad36a379e0cd5a748
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-03 02:36:31 +0400

    update TeacherController class

--- src/main/java/com/example/universitycourseregistration/controller/TeacherController.java
+++ src/main/java/com/example/universitycourseregistration/controller/TeacherController.java
@@ -1,4 +1,57 @@
 package com.example.universitycourseregistration.controller;
 
+import com.example.universitycourseregistration.dto.TeacherRequestDto;
+import com.example.universitycourseregistration.dto.TeacherResponseDto;
+import com.example.universitycourseregistration.service.TeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    //CREATE
+    @PostMapping
+    public TeacherResponseDto create(@RequestBody @Valid TeacherRequestDto dto) {
+        return teacherService.createTeacher(dto);
+    }
+
+    //Read all
+    @GetMapping
+    public List<TeacherResponseDto> getAll() {
+        return teacherService.getAllTeachers();
+    }
+
+    //read by ID
+    @GetMapping("/{id}")
+    public TeacherResponseDto getById(@PathVariable Long id) {
+        return teacherService.getById(id);
+    }
+
+    //update
+    @PutMapping("/{id}")
+    public TeacherResponseDto update(@PathVariable Long id,
+                                     @RequestBody @Valid TeacherRequestDto dto) {
+        return teacherService.updateTeacher(id, dto);
+    }
+
+
+    //delete
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        teacherService.deleteTeacher(id);
+    }
+
+    //find By Department
+    @GetMapping("/department")
+    public List<TeacherResponseDto> findByDepartment(@RequestParam String department) {
+        return teacherService.findByDepartment(department);
+    }
+
 }

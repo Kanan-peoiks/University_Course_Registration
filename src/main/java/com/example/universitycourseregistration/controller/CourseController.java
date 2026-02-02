55c52f4da4ed80dbcddb2905c052aa48066b7dd9
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-03 02:37:34 +0400

    update CourseController class

--- src/main/java/com/example/universitycourseregistration/controller/CourseController.java
+++ src/main/java/com/example/universitycourseregistration/controller/CourseController.java
@@ -1,4 +1,14 @@
 package com.example.universitycourseregistration.controller;
 
+import com.example.universitycourseregistration.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class CourseController {
+
+    private final CourseService courseService;
 }
